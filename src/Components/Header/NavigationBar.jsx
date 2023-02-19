8df9588fb876de759da72e68188eae942b4f69b7
Bright Orton <ortonb110@gmail.com>
2023-02-19 16:35:13 +0000

    Navigation Bar Complete without issues

--- src/Components/Header/NavigationBar.jsx
+++ src/Components/Header/NavigationBar.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Hamburger from "./Hamburger";
+import style from "../../Styles/styles";
 
 export default function NavigationBar(props) {
     
@@ -11,15 +12,16 @@ export default function NavigationBar(props) {
             <h2 className="text-[2.8rem] uppercase ">the planets</h2>
             <div className="">
             <Hamburger menuIsOpen={props.menuIsOpen} setMenu={props.setMenu}/>
-            <ul className={`${props.menuIsOpen? "left-[0] " : ""}  gap-[33px] uppercase  transition-all ease-in-out duration-300 mt-[2.5rem] md:mt-0 flex flex-col md:flex-row md:left-0 md:static fixed left-[-100%] md:translate-x-0 md:flex md:w-fit md:h-fit w-screen bg-primary h-screen`}>
-                <li><Link to={"/"}><h4 className="md:opacity-[0.5] border-b md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100">mercury</h4></Link></li>
-                <li><Link to={"/venus"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">venus</h4></Link></li>
-                <li><Link to={"/earth"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">earth</h4></Link></li>
-                <li> <Link to={"/mars"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">mars</h4></Link> </li>
-                <li> <Link to={"/jupiter"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">jupiter</h4></Link> </li>
-                <li><Link to={"/saturn"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">saturn</h4></Link></li>
-                <li><Link to={"/uranus"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">uranus</h4></Link> </li>
-                <li> <Link to={"/neptune"}><h4 className="md:opacity-[0.5] md:hover:border-t-2 md:border-lightBlue transition-all ease-in-out duration-100">neptune</h4></Link> </li>
+            <ul className={`${props.menuIsOpen? "left-[0] " : ""} px-[2.4rem] md:px-0 gap-[20px] md:gap-[33px] uppercase ${style.transition} ${style.navMobile} ${style.navDesktop} flex flex-col md:flex-row md:left-0 md:static fixed left-[-100%] md:translate-x-0 md:flex md:w-fit md:h-fit w-screen bg-primary h-screen text-[1.5rem] md:text-[1.1rem]`}>
+                <li onClick={props.setMenu}><Link to={"/"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#979797] mr-[2.5rem]"></span> mercury</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/venus"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#F7CC7F] mr-[2.5rem]"></span> venus</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/earth"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#545BFE] mr-[2.5rem]"></span> earth</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/mars"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#FF6A45] mr-[2.5rem]"></span> mars</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/jupiter"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#ECAD7A] mr-[2.5rem]"></span> jupiter</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/saturn"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#FCCB6B] mr-[2.5rem]"></span> saturn</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/uranus"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#65F0D5] mr-[2.5rem]"></span> uranus</h4></Link></li>
+                <li onClick={props.setMenu}><Link to={"/neptune"}><h4 className="items-center  flex md:opacity-[0.5] pb-[15px] md:pb-0  border-b md:border-b-0 border-[#979797] md:hover:border-t-2 md:border-lightBlue  transition-all ease-in-out duration-100"><span className="md:hidden inline-block rounded-full w-[2rem] h-[2rem] bg-[#497EFA] mr-[2.5rem]"></span> neptune</h4></Link></li>
+                
             </ul>
             </div>
         </nav>
--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,3 +1,9 @@
-const styles = [
-    
-]
+const style = {
+    navMobile: `fixed top-[6rem] left-0 w-full  h-screen bg-[#070724]`,
+    navDesktop: `md:static md:max-w-none md:transform-none md:h-auto md:bg-transparent`,
+    transition: `transition-all ease-in-out duration-300`, 
+
+}
+
+
+export default style;
