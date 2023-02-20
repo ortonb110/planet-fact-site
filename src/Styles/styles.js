a70aa14f8fa7e690aaa1d313d7191bf192c88877
Orton Bright <ortonb110@gmail.com>
2023-02-20 23:34:03 +0100

    Working on the main content component

--- src/Styles/styles.js
+++ src/Styles/styles.js
@@ -1,5 +1,5 @@
 const style = {
-    navMobile: `fixed top-[6rem] left-0 w-full  h-screen bg-[#070724]`,
+    navMobile: `fixed top-[5.25rem] left-0 w-full  h-screen bg-[#070724]`,
     navDesktop: `md:static md:max-w-none md:transform-none md:h-auto md:bg-transparent`,
     transition: `transition-all ease-in-out duration-300`, 
 
